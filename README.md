2a46b7dbf3ea63fcf0460067992ff0e88a9294d5
Romeu Fonseca <romeu.hcf@gmail.com>
2018-11-15 12:16:04 -0200

    Adds foreman, fixes route, and added HtmlWebpackHarddiskPlugin

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+# Rails basis SPA app
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Rails 5 API app with webpacker
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+No views included
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment, config } = require('@rails/webpacker')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
+const { resolve } = require('path')
+
+environment.plugins.append('html',
+new HtmlWebpackPlugin({
+  inject: 'body',
+  alwaysWriteToDisk: true,
+  filename: 'index.html',
+  template: resolve('public', 'index.html')
+}))
+
+environment.plugins.append('hardisk', new HtmlWebpackHarddiskPlugin())
 
 module.exports = environment
