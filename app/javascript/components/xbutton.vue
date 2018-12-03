81b9746ec5f1ec721a2a8f4cba826c509797775c
Romeu Fonseca <romeu.hcf@gmail.com>
2018-12-03 15:29:51 -0200

    Font Awesome

--- app/javascript/components/xbutton.vue
+++ app/javascript/components/xbutton.vue
@@ -2,15 +2,25 @@
   <div :class="styleSet()" :style="{display: display}">
     <span class="title">[{{sensor.status}}] {{sensor.title}}     <span class="source">@ {{sensor.source}}</span>
 </span>
-    <button v-if="snoozable()" @click="snooze" style="float:right">Soneca ⏰</button>
+    <button v-if="snoozable()" @click="snooze" style="float:right">Soneca
+        <font-awesome-icon icon="bell-slash"></font-awesome-icon>
+    </button>
 
-    <span style="float: right;font-size:1.3em;" v-if="snoozed()">💤</span>
-    <span v-if="sensor.ephemeral" class="ephemeral">📌</span>
+    <span style="float: right;font-size:1.3em;" v-if="snoozed()">
+        <font-awesome-icon icon="bed"></font-awesome-icon>
+    </span>
+    <span v-if="sensor.ephemeral" class="ephemeral">
+        <font-awesome-icon icon="thumbtack"></font-awesome-icon>
+    </span>
     
     
     <span v-else>
-      <span v-if="isOld()">💔</span>
-      <span v-else>💓</span>
+      <span v-if="isOld()">
+        <font-awesome-icon icon="unlink"></font-awesome-icon>
+      </span>
+      <span v-else>
+        <font-awesome-icon icon="heartbeat"></font-awesome-icon>
+      </span>
     </span>
     <span class="updated_at">{{moment(sensor.updated_at).fromNow()}}</span>
     <span class="value">{{sensor.value }}</span>
@@ -20,7 +30,14 @@
 </template>
 
 <script>
+import Vue from 'vue';
 import moment from 'moment';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeartbeat, faThumbtack, faBellSlash,faUnlink, faBed } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHeartbeat, faThumbtack, faBellSlash, faUnlink, faBed)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 export default {
   props: ['sensor','display'],
